22795234aa01e19a9bc127593f46a13e1c2549fc
Matthew Weisker <mweisker27@gmail.com>
2023-06-14 21:24:17 -0700

    Lay down the groundwork for hex game

--- src/path/HexGame.jsx
+++ src/path/HexGame.jsx
@@ -1,10 +1,38 @@
 import HomeButton from '../components/HomeButton'
-import '../css/hexGame.css'
+import '../css/hexGame.css';
+
+import { useState, useEffect } from 'react'
 
 export default function HexGame() {
+  const [hex, setHex] = useState([]);
+  const [restart, setRestart] = useState(true);
+
+  const total = 3;
+
+  // const correct = Math.floor(Math.random() * total);
+
+  useEffect(() => {
+    const hexArray = [];
+    for (let i = 0; i < total; i++) {
+      const currHex = newHex();
+      hexArray.push(currHex);
+    }
+    setRestart(false);
+    setHex(hexArray);
+  }, [restart])
+
+
+
+  const newHex = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+  }
+
   return (
   <div className='hex-container'>
     <h1>Introducing the super awesome Hex Game!!!</h1>
+    { hex }
+    <button onClick={() => setRestart(true)}>Restart</button>
     < HomeButton />
   </div>
   );
