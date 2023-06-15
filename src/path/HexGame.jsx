7b5fc265f64852c35946ae07dff5abb61d14104b
Matthew Weisker <mweisker27@gmail.com>
2023-06-14 20:46:29 -0700

    Began creating hex game

--- src/path/HexGame.jsx
+++ src/path/HexGame.jsx
@@ -1,11 +1,10 @@
 import HomeButton from '../components/HomeButton'
+import '../css/hexGame.css'
 
 export default function HexGame() {
   return (
-  <div>
+  <div className='hex-container'>
     <h1>Introducing the super awesome Hex Game!!!</h1>
-    <h2>A dancing bear</h2>
-    <img src='https://m.media-amazon.com/images/I/518UZ9vHr1L._AC_UF894,1000_QL80_.jpg'></img>
     < HomeButton />
   </div>
   );
